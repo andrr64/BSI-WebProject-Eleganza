dabb450a9ba500bf9d533dbb88c47dc2cc033053
andrr64 <derzandrr03@gmail.com>
2024-04-28 19:19:09 +0700

    implementasi API baru

--- web/server/index.js
+++ web/server/index.js
@@ -2,7 +2,9 @@ import express from 'express';
 import cookieParser from 'cookie-parser'; // Optional, if using cookies
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import API_userRoute from './routes/user.route.js'
+import API_userRoute from './routes/user.route.js';
+import API_productRoute from './routes/product.route.js';
+import API_adminRoute from './routes/admin.route.js';
 import { serverResponse } from './controllers/response.controller.js';
 
 const app = express();
@@ -10,6 +12,7 @@ const vers = '1.0.0'
 dotenv.config();
 
 app.use(express.json()); 
+app.use(cookieParser());
 mongoose
     .connect(process.env.MONGODB)
     .then(() => {
@@ -22,9 +25,12 @@ mongoose
 app.listen(3000, () => {
   console.log('-----------------------------');
   console.log(`Elenza Server ${vers}`);
+  console.log(`Port: 3000`);
   console.log('-----------------------------');
 });
-app.use('/api/user', API_userRoute);
+app.use('/api/v1/user', API_userRoute);
+app.use('/api/v1/product', API_productRoute);
+app.use('/api/v1/admin', API_adminRoute);
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error'
