606d4b55abac1b6044756f2e8caf9009e9e5b8d9
andrr64 <derzandrr03@gmail.com>
2024-05-01 12:17:01 +0700

    penambahan beberapa fungsi baru penghandle respon

--- server/controllers/response.controller.js
+++ server/controllers/response.controller.js
@@ -5,3 +5,27 @@ export const serverResponse = (pStatus, code, data= 'OK') => {
         data
     }
 }
+
+export const serverInternalError = (res, msg = '') => {
+    return res.status(500).json(serverResponse(false, 500, `Internal Server Error ${msg}`));
+}
+
+export const serverNotFound = (res) => {    
+    return res.status(404).json(serverResponse(false, 404, 'Not Found'));
+}
+
+export const serverBadRequest = (res, data) => {
+    return res.status(400).json(serverResponse(false, 400, `${data? data : 'Bad Request'}`));
+}
+
+export const serverOk = (res, data) => {
+    return res.status(201).json(serverResponse(true, 201, data));
+}
+
+export const serverForbidden = (res) => {
+    return res.status(401).json(serverResponse(false, 401, `Forbidden`))
+}
+
+export const serverNotAcceptable = (res) => {
+    return res.status(406).json(serverResponse(false, 406, 'Not Acceptable'))
+}
--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import {createProduct, updateProduct} from '../controllers/product.controller.js';
+import {createProduct, getProduct, updateProduct} from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.post('/create', createProduct);
 router.post('/update/:id', updateProduct);
+router.get('/get/:id', getProduct);
 
 export default router;
