314634aaadba78a9c2ba0049b084facb6bab68c0
andrr64 <derzandrr03@gmail.com>
2024-05-02 01:17:49 +0700

    update product.conotroller.js
    - penambahan parameter collection pada fungsi _productDataFilter

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -6,11 +6,12 @@ const PRICE_WRONG = 2;
 const DISCOUNT_WRONG = 3;
 const STOCK_WRONG = 4;
 const PICTURE_EMPTY = 5;
+const COLLECTION_EMPTY = 6;
 
 export const createProduct = async (req, res, next) => {
     try {
         const { name, list_picture, price, discount, stock } = req.body;
-        const filterData = _productDataFilter(name, list_picture, price, discount, stock);
+        const filterData = _productDataFilter(name, list_picture, price, discount, stock, collection);
         if (filterData.code !== 0) return serverBadRequest(res, filterData.message);
         await new Product({
             name: name,
@@ -18,6 +19,7 @@ export const createProduct = async (req, res, next) => {
             price: price,
             discount: discount,
             stock: stock,
+            collection: collection,
             hidden: false,
             previous_version: 'null'
         }).save();
@@ -29,7 +31,7 @@ export const createProduct = async (req, res, next) => {
 
 export const updateProduct = async (req, res) => {
     try {
-        const { name, list_picture, price, discount, stock } = req.body;
+        const { name, list_picture, price, discount, stock, collection } = req.body;
         const filterData = _productDataFilter(name, list_picture, price, discount, stock);
         if (filterData.code !== 0) return serverBadRequest(res, filterData.message);
         
@@ -44,6 +46,7 @@ export const updateProduct = async (req, res) => {
             price: price,
             discount: discount,
             stock: stock,
+            collection: collection,
             hidden: false
         });
 
@@ -67,12 +70,18 @@ export const getProduct = async(req, res) => {
     }
 }
 
-const _productDataFilter = (name, list_picture, price, discount, stock) => {
+const _productDataFilter = (name, list_picture, price, discount, stock, collection) => {
     if (!name) {
         return {
             code: NAME_WRONG,
             message: 'Name is required!'
         };
+    }
+    if (!collection) {
+        return {
+            code: COLLECTION_EMPTY,
+            message: 'Collection is required!'
+        }
     }
     if(list_picture.length <= 0){
         return {
