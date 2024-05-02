d8eb4e1b42cb8db36dd5264ecb3c9ef39aceebfc
andrr64 <derzandrr03@gmail.com>
2024-05-02 15:22:22 +0700

    update App.jsx * memperbaiki struktur kode menjadi rute-rute * menambahkan navigation bar

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -1,18 +1,19 @@
-import { useState } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import NavigationBar from "./components/NavigationBar"
+import Homepage from "./pages/Homepage"
+import Man from "./pages/Collection/Man";
+import Woman from "./pages/Collection/Woman";
 
 function App() {
-  const [count, setState] = useState(0);
   return (
-    <div>
-      <div>
-        App
-      </div>
-      <div className="text-red-700">
-        <button onClick={() => setState(count + 1)}>
-          {count}
-        </button>
-      </div>
-    </div>
+    <BrowserRouter>
+    <NavigationBar/>
+      <Routes>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='/collection/man' element={<Man/>} />
+        <Route path='/collection/woman' element={<Woman/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
