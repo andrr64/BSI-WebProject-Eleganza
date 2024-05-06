0f22f5d45fe994632a6d58551d3a6e709baa8c3e
andrr64 <derzandrr03@gmail.com>
2024-05-06 23:15:43 +0700

    update 'App.jsx'

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -1,20 +1,41 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
-import NavigationBar from "./components/NavigationBar"
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import Man from "./pages/Collection/Man";
 import Woman from "./pages/Collection/Woman";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import NavigationBar from "./components/NavigationBar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
-    <NavigationBar/>
+      <NavigationBarControl />
       <Routes>
-        <Route path='/' element={<Homepage/>} />
-        <Route path='/collection/man' element={<Man/>} />
-        <Route path='/collection/woman' element={<Woman/>} />
+        <Route path='/' element={<Homepage />} />
+        <Route path='/signin' element={<SignIn />} />
+        <Route path='/signup' element={<SignUp />} />
+        <Route path='/collection/man' element={<Man />} />
+        <Route path='/collection/woman' element={<Woman />} />
       </Routes>
+      <FooterControl />
     </BrowserRouter>
   )
 }
 
-export default App
+function FooterControl(){
+  const location = useLocation();
+  if (location.pathname === '/signin' || location.pathname === '/signup'){
+    return null;
+  }
+  return <Footer/>
+}
+
+function NavigationBarControl() {
+  const location = useLocation();
+  if (location.pathname === '/signin' || location.pathname === '/signup') {
+    return null;
+  }
+  return <NavigationBar />;
+}
+export default App;
