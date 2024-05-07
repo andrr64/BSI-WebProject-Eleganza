0580c628b24968b448df525a18b35d2c7cd31d65
andrr64 <derzandrr03@gmail.com>
2024-05-08 01:35:47 +0700

    update 'user.controller.js' ! implement server log and fix bug

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,10 +6,10 @@ import {newAccountValidation} from '../validation/user.account.validation.js';
 import { MESSAGE, serverLog } from './server.log.controller.js';
 
 export const createUser = async(req, res, next) => {
-    const title = 'create user';
+    const title = 'CONTROLLERS.USER.CONTROLLER.createUser';
     try {
         serverLog(MESSAGE.STARTOF_REQUEST, `${MESSAGE.STARTOF_REQUEST} [${title  }]`);
-        const {name, email, password, picture } = req.body;
+        const {name, email, password } = req.body;
         const validation = await newAccountValidation(name, email, password); 
         if (validation !== true) {
             serverLog(MESSAGE.FAILURE, validation)
@@ -23,15 +23,16 @@ export const createUser = async(req, res, next) => {
             name,
             email,
             password: hashPassword,
-            picture
+            picture: null
         })
         await newUser.save();
-        serverLog(MESSAGE.OK, 'New account created');
+        const {password: pass, ...rest} = newUser._doc;
+        serverLog(MESSAGE.OK, `NEW ACCOUNT CREATED: ${newUser.email}`);
         serverLog(MESSAGE.SENDING);
-        serverOk(res);
-        serverOk(MESSAGE.OK);
+        serverOk(res, rest);
+        serverLog(MESSAGE.OK);
+        serverLog(MESSAGE.ENDOF_REQUEST);
         return;
-
     } catch (error) {
         next(error);
     }
@@ -39,45 +40,55 @@ export const createUser = async(req, res, next) => {
 
 export const loginUser = async (req, res, next) => {
     try {
+        serverLog(MESSAGE.STARTOF_REQUEST, 'CONTROLLERS.USER.CONTROLLER.loginUser');
         const { email, password } = req.body; // Mendapatkan email dan password dari body request
-
         // Periksa apakah email dan password tersedia
         if (!email || !password) {
-            return res.status(400).json(serverResponse(false, 400, 'Email and password are required'));
+            serverLog(MESSAGE.FAILURE, 'Email and Password are empty.');
+            serverLog(MESSAGE.SENDING);
+            res.status(400).json(serverResponse(false, 400, 'Email and password are required'))
+            serverLog(MESSAGE.ENDOF_REQUEST);
+            return;
         }
 
         // Mencari pengguna berdasarkan email di database
-        const validUser = await UserAccount.findOne({ email });
-
+        const validUser = await UserAccount.findOne({ email: email });
         // Jika pengguna tidak ditemukan, kirim respons dengan status 404
         if (!validUser) {
             return res.status(404).json(serverResponse(false, 404, 'User not found'));
         }
-
+        serverLog(MESSAGE.ACCEPT, 'ACCOUNT OK!')
         // Memeriksa apakah password yang diberikan cocok dengan password yang tersimpan di database
+        serverLog(MESSAGE.VALIDATING, 'VALIDATING PASSWORD...')
         const passwordIsValid = bcryptjs.compareSync(password, validUser.password);
-
         // Jika password tidak cocok, kirim respons dengan status 401
         if (!passwordIsValid) {
+            serverLog(MESSAGE.FAILURE, 'PASSWORD NOT OK!');
+            serverLog(ENDOF_REQUEST);
             return res.status(401).json(serverResponse(false, 401, 'Incorrect password'));
         }
-
+        serverLog(MESSAGE.ACCEPT, 'PASSWORD OK!')
         // Jika login berhasil, menghasilkan token JWT
+        serverLog(MESSAGE.SENDING, 'SENDING TOKEN...');
         const token = jwt.sign({ email: validUser.email }, process.env.JWT_SECRET, { expiresIn: '1h' });
         // Kirim respons sukses dengan token JWT dan informasi pengguna yang terbatas
-        return res
+        serverLog(MESSAGE.ACCEPT, `TOKEN OK!`)
+        serverLog(MESSAGE.ACCEPT, `${email} LOGIN ACCEPTED`)
+        serverLog(MESSAGE.SENDING, 'SENDING RESPONSE...');
+        const {password: pass, ...rest} = validUser._doc;
+        res
             .status(200)
             .cookie('access_token', token, {httpOnly: true})
             .json(
                 serverResponse(true, 200, 
                     { 
-                        user: { 
-                            email: validUser.email, 
-                            picture: validUser.picture 
-                        } 
+                        user: rest
                     }
             )
         );
+        serverLog(MESSAGE.ACCEPT, 'RESPONSE OK!');
+        serverLog(MESSAGE.ENDOF_REQUEST);
+        return;
     } catch (error) {
         // Tangani kesalahan jika terjadi
         console.error('Error logging in user:', error);
