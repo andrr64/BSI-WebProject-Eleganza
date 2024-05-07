ce5334e03439aeb1eea29a293f3973bdb2b9430c
andrr64 <derzandrr03@gmail.com>
2024-05-08 01:36:42 +0700

    update 'SignUp.jsx' + implementasi fungsi registrasi akun dan validasi form

--- web/client/src/pages/SignUp.jsx
+++ web/client/src/pages/SignUp.jsx
@@ -1,37 +1,109 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import { faChevronLeft} from "@fortawesome/free-solid-svg-icons"
+import GoogleLogo from "../assets/icons/brands/google.svg";
+import { serverApiJsonPost } from "../api/API";
 
-const buatField = (label, idField, type, placeholder, valuePtr, onChange) => {
+const buatField = (label, idField, type, placeholder, onChange, isError, errorText, callback = undefined) => {
   return (
     <div>
-      <label htmlFor={idField} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+      <label htmlFor={idField} className={`block mb-2 text-sm font-medium ${isError ? 'text-red-600' : 'text-gray-900 dark:text-white'}`}>
+        {label}
+        {isError && <span className="ml-1 font-bold">*</span>}
+      </label>
       <input 
         type={type} 
         name={idField}
         id={idField}
-        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        placeholder={placeholder} 
-        value={valuePtr}
-        onChange={onChange}
+        className={`bg-gray-50 border ${isError ? 'border-red-600' : 'border-gray-300'} text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
+        placeholder={placeholder}   
+        onChange={(e) => {
+          onChange(e);
+          if (callback !== undefined){
+            callback();
+          }
+        }}
       />
+      {isError && <p className="text-red-600 text-sm mt-1">{errorText}</p>}
     </div>
   )
 }
 
 const SignUp = () => {
-  const [nama, setNama] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const navigator = useNavigate();
+  const [formData, setFormData] = useState({});
+  const [nameError, setNameError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [nameErrorMsg, setNameErrorMsg] = useState('');
+  const [emailErrorMsg, setEmailErrorMsg] = useState('');
+  const [passwordErrorMsg, setPasswordErrorMsg] = useState('');
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+  const validateForm = () => {
+    let isValid = true;
+    // Validasi Nama
+    if (!formData.name) {
+      setNameError(true);
+      setNameErrorMsg('Nama tidak boleh kosong!');
+      isValid = false;
+    } else if (!/^[a-zA-Z\s]*$/.test(formData.name)) {
+      setNameError(true);
+      setNameErrorMsg('Nama tidak boleh mengandung simbol atau angka!');
+      isValid = false;
+    }
+    
+    // Validasi email
+    if (!formData.email){
+      setEmailError(true);
+      setEmailErrorMsg('Email tidak boleh kosong');
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      setEmailError(true);
+      setEmailErrorMsg('Masukkan alamat email yang valid!');
+      isValid = false;
+    }
+
+    // Validasi Password
+    if (!formData.password || formData.password.length < 8) {
+      setPasswordError(true);
+      setPasswordErrorMsg('Password harus memiliki minimal 8 karakter!');
+      isValid = false;
+    }
   
+    return isValid;
+  };
+  const handleSubmit = async () => {
+    if (!validateForm()) return;
+  
+    // Option 1: Don't set picture to null if not needed
+    // setFormData({...formData, picture: null}); // Remove this line if not sending picture
+  
+    // Option 2: Send an empty string for picture if needed
+    setFormData({ ...formData, picture: null });
+    try {
+      const res = await serverApiJsonPost('/user/create', formData);
+      const responseJson = await res.json();
+      if (responseJson.status){
+        navigator('/');
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div className="font-inter flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <Link to={'/'}>
-              <div className="text-sm duration-300 transition ease-in-out font-medium flex items-center hover:-translate-x-2">
+              <div className="text-sm duration-300 transition ease-in-out font-bold flex items-center hover:-translate-x-2">
                 <FontAwesomeIcon icon={faChevronLeft} className="mr-2"/>
                 <p>Kembali ke Beranda</p>
               </div>
@@ -45,20 +117,27 @@ const SignUp = () => {
               Registrasi Akun
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
-              {buatField('Nama','nama', 'text', 'Nama Lengkap', nama, (e) => setNama(e.target.value))}
-              {buatField('Email', 'email', 'email', 'contoh@email.com', email, (e) => setEmail(e.target.value))}
-              {buatField('Password', 'password', 'password', '••••••••', password, (e) => setPassword(e.target.value))}
+              {buatField('Nama','name', 'text', 'Nama Lengkap', handleChange, nameError, nameErrorMsg, () => setNameError(false))}
+              {buatField('Email', 'email', 'email', 'contoh@email.com', handleChange, emailError, emailErrorMsg, () => setEmailError(false))}
+              {buatField('Password', 'password', 'password', '••••••••', handleChange, passwordError, passwordErrorMsg, () => setPasswordError(false))}
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Sudah memiliki akun? <a href="/signin" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Masuk</a>
               </p>
-              <div>
-                <button
-                  type="submit"
-                  className="duration-300 hover:-translate-y-1 transition ease-in-out delay-150 flex w-full justify-center rounded-md bg-gray-800 hover:bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              <button
+                type="button"
+                onClick={handleSubmit}
+                className="duration-300 hover:-translate-y-1 transition ease-in-out delay-150 flex w-full justify-center rounded-md bg-gray-800 hover:bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                <span className="my-1">Buat Akun</span>
+              </button>
+                <p className="text-center text-sm">atau</p>
+              <button
+                  type="button"
+                  className="text-black duration-300 hover:-translate-y-1 transition ease-in-out delay-150 flex w-full justify-center rounded-md border bg-transparent hover:bg-gray-100 px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm "
                 >
-                  Buat Akun
+                  <img src={GoogleLogo} alt="Google Logo" className="w-6 h-6 mr-2" />
+                  Daftar dengan Google
                 </button>
-              </div>
             </form>
           </div>
         </div>
