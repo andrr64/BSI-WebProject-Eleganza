86ba51589f4d9617fd361509ca4b7beedf78d721
andrr64 <derzandrr03@gmail.com>
2024-05-09 01:19:22 +0700

    add AppRoute.jsx

---
+++ web/client/src/AppRoute.jsx
@@ -0,0 +1,10 @@
+export const ROUTE = {
+    homepage: '/',
+    man: '/man',
+    woman: '/woman',
+    user: {
+        signin: '/user/signin',
+        signup: '/user/signup',
+        profile: '/user/profile'
+    }
+}
