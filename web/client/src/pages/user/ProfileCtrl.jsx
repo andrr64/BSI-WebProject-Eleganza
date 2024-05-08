0b0689db956246dabed4c7a5d2cb56d13dffa586
andrr64 <derzandrr03@gmail.com>
2024-05-09 02:04:30 +0700

    implementasi validasi akses token

--- web/client/src/pages/user/ProfileCtrl.jsx
+++ web/client/src/pages/user/ProfileCtrl.jsx
@@ -1,11 +1,28 @@
-import { useSelector } from "react-redux"
-import {Outlet, Navigate } from "react-router-dom";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Outlet, Navigate } from "react-router-dom";
 import { ROUTE } from "../../AppRoute";
+import { serverApiJsonGet, serverApiJsonPost } from "../../api/API";
+import { signOutSuccess } from "../../redux/user/userSlice";
 
 export default function ProfileCtrl() {
-  const {currentUser} = useSelector((state) => state.user);
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
-  return currentUser?
-    <Outlet/>: 
-    <Navigate to={ROUTE.user.signin} />
+  useEffect(() => {
+    const isValidToken = async () => {
+      try {
+        const res = await(await serverApiJsonGet('/user/check-token')).json();
+        if (!res.status) {
+          dispatch(signOutSuccess());
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    isValidToken(); // Memanggil fungsi isValidToken saat komponen dipasang
+  }, []); // Dependensi kosong agar efek ini dijalankan sekali saat komponen dipasang
+
+  return currentUser ? <Outlet /> : <Navigate to={ROUTE.user.signin} />;
 }
