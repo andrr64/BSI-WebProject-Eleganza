75216aa5a366edd70970f02a0421c9b570412696
andrr64 <derzandrr03@gmail.com>
2024-05-09 01:27:33 +0700

    penambahan validasi form

--- web/client/src/pages/user/SignIn.jsx
+++ web/client/src/pages/user/SignIn.jsx
@@ -8,50 +8,87 @@ import { signInStart, signInSuccess, signInFailure } from "../../redux/user/user
 
 import GoogleLogo from "../../assets/icons/brands/google.svg";
 
-const buatField = (label, idField, type, placeholder, valuePtr, onChange) => {
+const buatField = (label, idField, type, placeholder, valuePtr, onChange, isError, errorMessage) => {
   return (
     <div>
-      <label htmlFor={idField} className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{label}</label>
+      <label htmlFor={idField} className={`block mb-2 text-sm font-medium ${isError ? 'text-red-600' : 'text-gray-900 dark:text-white'}`}>
+        {label}
+      </label>
       <input 
         type={type} 
         name={idField}
         id={idField}
-        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        className={`bg-gray-50 border ${isError ? 'border-red-600' : 'border-gray-300'} text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500`}
         placeholder={placeholder} 
         value={valuePtr}
         onChange={onChange}
       />
+      {isError && <p className="text-red-600 text-sm mt-1">{errorMessage}</p>}
     </div>
   )
 }
 
 const SignIn = () => {
   const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState({
+    error: false,
+    message: ''
+  })
+  const [passwordError, setPasswordError] = useState({
+    error: false,
+    message: ''
+  })
   const [password, setPassword] = useState('');
   const navigator = useNavigate();
   const dispatch = useDispatch();
+
+  // Handle Enter key press on email or password field
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      document.getElementById("loginButton").click();
+    }
+  };
+  const validateForm = () => {
+    let isValid = true;
+    if (!password){
+      setPasswordError({
+        error: true,
+        message: 'Password is empty!'
+      })
+      isValid = false;
+    }
+    if (!email){
+      setEmailError({
+        error: true,
+        message: 'Email is empty!'
+      })
+      isValid = false;
+    }
+    return isValid;
+  }
   const handleSubmit = async () => {
+    if (!validateForm()) return;
     dispatch(signInStart());
-    const response = await(await serverApiJsonPost('/user/login',{
+    const response = await (await serverApiJsonPost('/user/login', {
       email: email,
       password: password
     })).json();
-    if (response.status){
+    if (response.status) {
       dispatch(signInSuccess(response.data.user));
       navigator('/');
       return;
     }
     dispatch(signInFailure('Login Fail...'));
     return;
-  }
+  };
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
-      <div className="font-inter flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div id="form" className="font-inter flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <Link to={'/'} className="transition ease-in-out duration-300 flex hover:-translate-x-2 items-center font-bold text-sm">
-              <FontAwesomeIcon icon={faChevronLeft} className="mr-2"/>
+              <FontAwesomeIcon icon={faChevronLeft} className="mr-2" />
               <p>Kembali ke beranda</p>
             </Link>
             <div>
@@ -63,8 +100,28 @@ const SignIn = () => {
               Masuk
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
-              {buatField('Email', 'email', 'email', 'user@email.com', email, (e) => setEmail(e.target.value))}
-              {buatField('Password', 'password', 'password', '••••••••', password, (e) => setPassword(e.target.value))}
+              <div onKeyDown={(handleKeyDown)}>
+                {buatField('Email', 'email', 'email', 'user@email.com', email, (e) => {
+                  setEmail(e.target.value);
+                  if (emailError.error === true){
+                    setEmailError({
+                      error: false,
+                      message: ''
+                    })
+                  }
+                }, emailError.error, emailError.message)}
+              </div>
+              <div onKeyDown={handleKeyDown}>
+                {buatField('Password', 'password', 'password', '••••••••', password, (e) => {
+                  setPassword(e.target.value);
+                  if (passwordError.error === true){
+                    setPasswordError({
+                      error: false,
+                      message: ''
+                    })
+                  }
+                }, passwordError.error, passwordError.message)}
+              </div>
               <div className="flex items-center justify-between">
                 <div className="flex items-start">
                   <div className="flex items-center h-5">
@@ -78,6 +135,7 @@ const SignIn = () => {
               </div>
               <div className="space-y-3">
                 <button
+                  id="loginButton"
                   onClick={handleSubmit}
                   type="button"
                   className="duration-300 hover:-translate-y-1 transition ease-in-out delay-150 flex w-full justify-center rounded-md bg-gray-800 hover:bg-gray-900 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -104,6 +162,6 @@ const SignIn = () => {
       </div>
     </section>
   );
-}
+};
 
 export default SignIn;
