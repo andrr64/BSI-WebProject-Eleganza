dbf472915704d7c3ba9793fc8e027fb392111cfb
andrr64 <derzandrr03@gmail.com>
2024-05-09 01:48:12 +0700

    rename 'Profile' to 'ProfileButton'

--- web/client/src/components/navbar/ProfileButton.jsx
+++ web/client/src/components/navbar/ProfileButton.jsx
@@ -3,11 +3,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faUser } from "@fortawesome/free-regular-svg-icons"
 import { ROUTE } from "../../AppRoute"
 
-export default function Profile() {
+export default function ProfileButton() {
     return (    
     <div className="transition hover:translate-y-1 ease-in-out">
-        <Link to={ROUTE.user.signup}>
-        <FontAwesomeIcon icon={faUser} color="white" size="lg"/>
+        <Link to={ROUTE.user.profile}>
+         <FontAwesomeIcon icon={faUser} color="white" size="lg"/>
         </Link>
     </div>
     )
