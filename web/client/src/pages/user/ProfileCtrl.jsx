80058000108a1258f2fc318816c11f26f7e6203c
andrr64 <derzandrr03@gmail.com>
2024-05-09 01:25:28 +0700

    implementasi ROUTE pada ProfileCtrl dan SignXCtrl

--- web/client/src/pages/user/ProfileCtrl.jsx
+++ web/client/src/pages/user/ProfileCtrl.jsx
@@ -1,10 +1,11 @@
 import { useSelector } from "react-redux"
 import {Outlet, Navigate } from "react-router-dom";
+import { ROUTE } from "../../AppRoute";
 
 export default function ProfileCtrl() {
   const {currentUser} = useSelector((state) => state.user);
 
   return currentUser?
     <Outlet/>: 
-    <Navigate to={'/signin'} />
+    <Navigate to={ROUTE.user.signin} />
 }
--- web/client/src/pages/user/SignXCtrl.jsx
+++ web/client/src/pages/user/SignXCtrl.jsx
@@ -1,9 +1,10 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import { ROUTE } from "../../AppRoute";
 
 export function SignXCtrl () {
   const {currentUser} = useSelector((state) => state.user);
   return currentUser?
-    <Navigate to={'/profile'}/>:
+    <Navigate to={ROUTE.user.profile}/>:
     <Outlet/>
 }
