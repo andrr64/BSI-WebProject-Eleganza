b1e353e1e44ff18efe8e404bad8fdcdefe99447c
andrr64 <derzandrr03@gmail.com>
2024-05-08 12:53:03 +0700

    Update 'App.jsx'

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -2,10 +2,13 @@ import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
 import Homepage from "./pages/Homepage"
 import Man from "./pages/Collection/Man";
 import Woman from "./pages/Collection/Woman";
-import SignIn from "./pages/SignIn";
-import SignUp from "./pages/SignUp";
+import SignIn from "./pages/user/SignIn";
+import SignUp from "./pages/user/SignUp";
 import NavigationBar from "./components/NavigationBar";
 import Footer from "./components/Footer";
+import ProfileCtrl from "./pages/user/ProfileCtrl";
+import Profile from "./pages/user/Profile";
+import { SignXCtrl } from "./pages/user/SignXCtrl";
 
 function App() {
   return (
@@ -13,10 +16,15 @@ function App() {
       <NavigationBarControl />
       <Routes>
         <Route path='/' element={<Homepage />} />
-        <Route path='/signin' element={<SignIn />} />
-        <Route path='/signup' element={<SignUp />} />
         <Route path='/collection/man' element={<Man />} />
         <Route path='/collection/woman' element={<Woman />} />
+        <Route element={<SignXCtrl />}>
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+        </Route>
+        <Route element={<ProfileCtrl />}>
+          <Route path='/profile' element={<Profile/>} />
+        </Route>
       </Routes>
       <FooterControl />
     </BrowserRouter>
@@ -25,7 +33,9 @@ function App() {
 
 function FooterControl(){
   const location = useLocation();
-  if (location.pathname === '/signin' || location.pathname === '/signup'){
+  if (
+    location.pathname === '/signin' || 
+    location.pathname === '/signup'){
     return null;
   }
   return <Footer/>
