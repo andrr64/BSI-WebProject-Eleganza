69b37d3de0b4c0e072ac29cbb1d54f9fbda6d9d9
andrr64 <derzandrr03@gmail.com>
2024-05-08 12:52:23 +0700

    menghapus 'searchbar' dan perapihan kode

--- web/client/src/components/NavigationBar.jsx
+++ web/client/src/components/NavigationBar.jsx
@@ -1,25 +1,18 @@
-import { useState } from "react"
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {faUser} from "@fortawesome/free-solid-svg-icons"
 
-import SearchBar from "./SearchBar"
-
 function ProfileController () {
   return (
-    <Link to={'/signin'}>
-      <FontAwesomeIcon icon={faUser} color="white" size="xl" className="ml-8"/>
-    </Link>
+    <div className="transition hover:translate-y-1 ease-in-out">
+      <Link to={'/profile'}>
+        <FontAwesomeIcon icon={faUser} color="white" size="xl" className="ml-8"/>
+      </Link>
+    </div>
   )
 }
 
 const NavigationBar = () => {
-  const [activeMenu, setActiveMenu] = useState('Beranda');
-
-  const handleMenuClick = (menu) => {
-    setActiveMenu(menu);
-  };
-
   const navBarMenu = {
     menu: [
       'Beranda',
@@ -37,15 +30,14 @@ const NavigationBar = () => {
     <nav className="bg-black text-white flex items-center px-20 py-5">
       <div className="flex items-center flex-grow">  {/* Flex-grow for spacing */}
         <h1 className="text-4xl font-medium ml-4 font-laBelle">
-          <Link to="/">Eleganza</Link>
+          Eleganza
         </h1>
         <div className="hidden md:flex"> {/* Move menu to right side */}
-          <ul className="px-12 flex item-center space-x-10 font-inter">
+          <ul className="px-12 flex item-center space-x-10">
             {navBarMenu.menu.map((menu, index) => (
               <li
                 key={index}
-                className={`${activeMenu === menu ? 'font-inter font-bold' : 'font-inter-light'} uppercase`}
-                onClick={() => handleMenuClick(menu)}
+                className={`font-medium hover:translate-y-1 uppercase transition ease-in-out`}
                 style={{ cursor: 'pointer' }}
               >
                 <Link to={navBarMenu.linkTo[index]}>{menu}</Link>
@@ -54,7 +46,6 @@ const NavigationBar = () => {
           </ul>
         </div>
       </div>
-      <SearchBar />
       <ProfileController/>
     </nav>
   );
