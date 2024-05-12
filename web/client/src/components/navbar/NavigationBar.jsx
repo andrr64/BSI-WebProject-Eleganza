70ce24d3deebba9b4212a75c98fd2362f68f0ab4
andrr64 <derzandrr03@gmail.com>
2024-05-13 01:07:36 +0700

    update NavigationBar.jsx

--- web/client/src/components/navbar/NavigationBar.jsx
+++ web/client/src/components/navbar/NavigationBar.jsx
@@ -3,6 +3,7 @@ import './NavigationBar.css'
 import SearchBar from "./SearchBar"
 import ProfileButton from "./ProfileButton";
 import { ROUTE } from "../../AppRoute";
+import MenuBar from "./MenuBar";
 
 const NavigationBar = () => {
   const createNavBarItem = (title, path) => {
@@ -18,11 +19,13 @@ const NavigationBar = () => {
   ]
 
   return (
-    <nav className="bg-black text-white flex items-center px-20 py-5 navigationBar">
+    <nav className="bg-black text-white py-5 px-8 md:px-10 lg:px-14 flex items-center navigationBar">
       <div className="flex items-center flex-grow">  {/* Flex-grow for spacing */}
-        <h1 className=" text-4xl font-medium ml-4 font-laBelle">
-          Eleganza
-        </h1>
+        <Link to={ROUTE.homepage}>
+          <h1 className="text-2xl lg:text-4xl font-medium font-laBelle">
+            Eleganza
+          </h1>
+        </Link>
         <div className="font-inter hidden md:flex"> {/* Move menu to right side */}
           <ul className="px-12 flex item-center space-x-10">
             {navbarItems.map((item, index) => (
@@ -40,6 +43,9 @@ const NavigationBar = () => {
       <ul className="flex space-x-6">
         <li><SearchBar/></li>
         <li><ProfileButton/></li>
+        <li className="md:hidden">
+          <MenuBar/>
+        </li>
       </ul>
     </nav>
   );
