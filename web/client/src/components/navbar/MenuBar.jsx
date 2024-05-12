6da003bf8c8182c75541c5354fd641c6e13575e9
andrr64 <derzandrr03@gmail.com>
2024-05-13 01:05:55 +0700

    add MenuBar.jsx

---
+++ web/client/src/components/navbar/MenuBar.jsx
@@ -0,0 +1,10 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+export default function MenuBar() {
+  return (
+    <>
+        <FontAwesomeIcon icon={faBars} color="white" size="lg"/>
+    </>
+  )
+}
