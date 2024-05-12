df28cd4f97df2465eeb7d0a7226a594219bfcd8f
andrr64 <derzandrr03@gmail.com>
2024-05-13 01:08:46 +0700

    Update logika fungsi isValidToken

--- web/client/src/pages/user/ProfileCtrl.jsx
+++ web/client/src/pages/user/ProfileCtrl.jsx
@@ -2,13 +2,12 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 import { ROUTE } from "../../AppRoute";
-import { serverApiJsonGet, serverApiJsonPost } from "../../api/API";
+import { serverApiJsonGet } from "../../api/API";
 import { signOutSuccess } from "../../redux/user/userSlice";
 
 export default function ProfileCtrl() {
   const { currentUser } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-
   useEffect(() => {
     const isValidToken = async () => {
       try {
@@ -17,12 +16,12 @@ export default function ProfileCtrl() {
           dispatch(signOutSuccess());
         }
       } catch (error) {
-        console.log(error);
+        console.log();
       }
     };
-
-    isValidToken(); // Memanggil fungsi isValidToken saat komponen dipasang
-  }, []); // Dependensi kosong agar efek ini dijalankan sekali saat komponen dipasang
-
+    if (currentUser !== null){
+      isValidToken();
+    }
+  }, []);
   return currentUser ? <Outlet /> : <Navigate to={ROUTE.user.signin} />;
 }
