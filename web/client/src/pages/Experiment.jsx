1c9450d0942bc7bf972ddbf680863de56dfa2cf1
andrr64 <derzandrr03@gmail.com>
2024-05-15 01:31:19 +0700

    idk

--- web/client/src/pages/Experiment.jsx
+++ web/client/src/pages/Experiment.jsx
@@ -1,13 +1,5 @@
 export default function Experiment() {
-    return (
-      <div className="flex">
-        <div id="left-side" className="w-2/3 bg-gray-50 h-screen">
-
-        </div>
-        <div id="content" className="w-1/2 bg-gray h-screen">
-
-        </div>
-      </div>
-    );
-  }
-  
+  return (
+    <div>Experiment</div>
+  )
+}
