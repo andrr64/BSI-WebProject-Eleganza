c1cdde9995996c76c6fc4942760b0c2a86e03440
andrr64 <derzandrr03@gmail.com>
2024-05-14 18:52:12 +0700

    update Homepage.jsx

--- web/client/src/pages/Homepage.jsx
+++ web/client/src/pages/Homepage.jsx
@@ -11,48 +11,61 @@ function buildCollectionCard(link, imageUrl, title, extraClassname) {
         transition duration-500 ease-in-out 
         flex justify-center items-center
         relative
-        bg-[url('${imageUrl}')]
-        bg-cover bg-top
         ${extraClassname}
       `}
     >
+      <img 
+        src={imageUrl}
+        alt={title}
+        className="absolute inset-0 w-full h-full object-cover"
+      />
       <div
         className="
           absolute inset-0
           bg-black opacity-20 hover:opacity-40 transition duration-500 ease-in-out
         "
       ></div>
-      <p 
+      <div 
         className="z-10 text-center text-white font-inter text-3xl">
         {title}
-      </p>
+      </div>
     </Link>
-  )
+  );
 }
-
 function buildUnggulanCard(link, imageUrl, title, extraClassname) {
   return (
-      <Link 
-        to={link}
-        className={`
-          homepage-unggulan
-          bg-[url('${imageUrl}')]
-          flex justify-center items-center
-          relative
-          hover:-translate-y-2 transition duration-500 ease-in-out 
-          ${extraClassname}
-        `}
-      >
-        <div
+    <Link 
+      to={link}
+      className={`
+        homepage-unggulan
+        flex justify-center items-center
+        relative
+        transition duration-500 ease-in-out
+        hover:scale-95
+        border-2 border-white rounded-lg
+        overflow-hidden
+        ${extraClassname}
+      `}
+    >
+      <img 
+        src={imageUrl}
+        alt={title}
+        className="absolute inset-0 w-full h-full object-cover"
+      />
+      <div
         className="
           absolute inset-0
-          bg-black opacity-5 hover:opacity-20 transition duration-500 ease-in-out
+          bg-black opacity-10 hover:opacity-30 transition duration-500 ease-in-out
         "
       ></div>
-      <p className="z-10 text-white text-3xl">{title}</p>
-      </Link>
-  )
+      <div className="justify-center items-center flex backdrop-blur-sm border py-1 px-5 z-10 text-white bg-white bg-opacity-30">
+        <p className="text-3xl text-center">{title}</p>
+      </div>
+    </Link>
+  );
 }
+
+
 
 function Homepage() {
   return (
@@ -87,7 +100,10 @@ function Homepage() {
           </div>
           <div className="grid justify-center items-center  grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-2">
             {buildUnggulanCard("https://google.com", "https://assets.vogue.com/photos/6407afdd068d1267137bb20d/4:3/w_840,h_630,c_limit/00-social.jpg", "Bags", "")}
-            {buildUnggulanCard("https://i.pinimg.com/564x/ba/6a/08/ba6a081b2a989593600fdaad28b5524f.jpg", "Man", "")}
+            {buildUnggulanCard("https://google.com", "https://i.pinimg.com/564x/ba/6a/08/ba6a081b2a989593600fdaad28b5524f.jpg", "Man", "")}
+            {buildUnggulanCard("https://google.com", "https://pinoy-in-style.com/wp-content/uploads/2020/11/1-819x1024.jpg", "Women", "")}
+            {buildUnggulanCard("https://google.com", "https://img.freepik.com/free-photo/kids-fashion-style-childrens-wear-accessories-concept-serious-confident-african-american-boy-modeling-against-blank-wall-wearing-striped-jumper-pink-shades-his-head_343059-4497.jpg", "Kids", "")}
+            {buildUnggulanCard("https://google.com", "https://pinoy-in-style.com/wp-content/uploads/2020/11/1-819x1024.jpg", "Women", "")}
             {buildUnggulanCard("https://google.com", "https://pinoy-in-style.com/wp-content/uploads/2020/11/1-819x1024.jpg", "Women", "")}
           </div>
         </section>
