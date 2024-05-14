0bd0a1adf8496cab817d98b105386edb0dcecff5
andrr64 <derzandrr03@gmail.com>
2024-05-15 01:30:53 +0700

    fix route

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
-import { SignXCtrl } from "./pages/user/SignXCtrl";
+import { SignInController } from "./pages/user/SignInController";
+import { SignUpController } from "./pages/user/SignUpController";
 import {ROUTE} from "./AppRoute";
 import Homepage from "./pages/Homepage"
 import Man from "./pages/Man";
 import Woman from "./pages/Woman";
-import SignIn from "./pages/user/SignIn";
-import SignUp from "./pages/user/SignUp";
-import ProfileCtrl from "./pages/user/ProfileCtrl";
-import Profile from "./pages/user/Profile";
+import ProfileController from "./pages/user/ProfileController";
 import NotFound from "./pages/NotFound";
 import Experiment from "./pages/Experiment";
 import NavigationBar from "./components/navbar/NavigationBar";
@@ -29,13 +27,9 @@ function App() {
         <Route path={ROUTE.homepage} element={<Homepage />} />
         <Route path={ROUTE.man} element={<Man />} />
         <Route path={ROUTE.woman} element={<Woman />} />
-        <Route element={<SignXCtrl />}>
-          <Route path={ROUTE.user.signin} element={<SignIn />} />
-          <Route path={ROUTE.user.signup} element={<SignUp />} />
-        </Route>
-        <Route element={<ProfileCtrl />}>
-          <Route path={ROUTE.user.profile} element={<Profile/>} />
-        </Route>
+        <Route path={ROUTE.user.signin} element={<SignInController />} />
+        <Route path={ROUTE.user.signup} element={<SignUpController />} />
+        <Route path={ROUTE.user.profile} element={<ProfileController/>} />
         <Route path='*' element={<NotFound/>} /> {/* Rute untuk URL tidak valid*/}
         <Route path="/experiment" element={<Experiment/>} />
       </Routes>
