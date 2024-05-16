8b2b0ff1b2b79a75ee272fd3b413f1344e98481f
andrr64 <derzandrr03@gmail.com>
2024-05-17 00:08:21 +0700

    add new route

--- web/client/src/AppRoute.jsx
+++ web/client/src/AppRoute.jsx
@@ -2,6 +2,7 @@ export const ROUTE = {
     homepage: '/',
     man: '/man',
     woman: '/woman',
+    children: '/children',
     user: {
         signin: '/user/signin',
         signup: '/user/signup',
