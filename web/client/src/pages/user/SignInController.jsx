959915f84ffeea77281f5fccd19b9188449ae95b
andrr64 <derzandrr03@gmail.com>
2024-05-18 03:21:53 +0700

    update SignInController.jsx

--- web/client/src/pages/user/SignInController.jsx
+++ web/client/src/pages/user/SignInController.jsx
@@ -1,11 +1,18 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
 import { ROUTE } from "../../AppRoute";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import SignIn from "./SignIn";
 
 export function SignInController () {
   const {currentUser} = useSelector((state) => state.user);
-  return currentUser === null?
-    <SignIn/>:
-    <Navigate to={ROUTE.homepage}/>;
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (currentUser !== null){
+      navigate(ROUTE.user.profile);
+      return; 
+    }
+  }, [])
+  
+  return <SignIn/>;
 }
