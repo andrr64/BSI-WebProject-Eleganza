dcd74460329746d1fc530121d465f01fc0656d2b
andrr64 <derzandrr03@gmail.com>
2024-05-18 03:22:24 +0700

    update CollectionCard.jsx + parameter + fungsi

--- web/client/src/components/CollectionCard.jsx
+++ web/client/src/components/CollectionCard.jsx
@@ -1,7 +1,23 @@
 import { Link } from "react-router-dom";
 
 // eslint-disable-next-line react/prop-types
-export function CollectionCard({link, imageUrl, title, extraClassname=null, font=null, height='100vh', fontSize="text-lg lg:text-3xl"}, uppercase=true) {
+export function CollectionCard({link, imageUrl, title, extraClassname=null, font=null, height='100vh', fontSize="text-lg lg:text-3xl", width="100%", scaleTransition = true, opacityTransition = true, uppercase=true, titleWithImage=false, titleWithImageWidth = "100%", titleWithImageHeight = "100%", opacity=5}) {
+ 
+  const getTitle = () => {
+    if (titleWithImage !== false){
+      return (
+        <img style={{
+          width: titleWithImageWidth,
+          height: titleWithImageHeight
+        }} src={titleWithImage} alt="" />
+      )
+    } else {
+      return (
+        <p className={`${font !== null? font : 'font-inter'} ${uppercase === true? 'uppercase' : ''} ${fontSize} text-center`}>{title}</p>
+      )
+    }
+  }
+
   return (
     <Link 
       to={link}
@@ -10,11 +26,11 @@ export function CollectionCard({link, imageUrl, title, extraClassname=null, font
         flex justify-center items-center
         relative
         transition duration-500 ease-in-out
-        hover:scale-95
-        ${extraClassname != null? extraClassname : ''}
-      `}
+        ${scaleTransition === false? '' : 'hover:scale-95'}
+        ${extraClassname !== null? extraClassname : ''}`}
       style={{
-        height: height
+        height: height,
+        width: width
       }}
     >
       <img 
@@ -23,13 +39,11 @@ export function CollectionCard({link, imageUrl, title, extraClassname=null, font
         className="absolute inset-0 w-full h-full object-cover object-top"
       />
       <div
-        className="
-          absolute inset-0
-          bg-black opacity-5 hover:opacity-30 transition duration-500 ease-in-out
-        "
+        className={`
+          absolute inset-0 bg-black opacity-10 ${opacityTransition? 'hover:opacity-30' : ''} transition duration-500 ease-in-out`}
       ></div>
       <div className="justify-center items-center flex py-2 px-6 z-10 text-white">
-        <p className={`${font !== null? font : 'font-inter'} ${uppercase === true? 'uppercase' : ''} ${fontSize} text-center`}>{title}</p>
+        {getTitle()}
       </div>
     </Link>
   );
