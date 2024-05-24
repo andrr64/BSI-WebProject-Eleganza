0040a9ba136232418f046619957fa407408b8554
andrr64 <derzandrr03@gmail.com>
2024-05-24 15:04:41 +0700

    menambahkan ruter baru: get/brand/:brandname

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createProduct, getProduct, updateProduct} from '../controllers/product.controller.js';
+import {createProduct, getProduct, getProductByBrand, updateProduct} from '../controllers/product.controller.js';
 
 const router = express.Router();
 
 router.post('/create', createProduct);
 router.post('/update/:id', updateProduct);
 router.get('/get/:id', getProduct);
+router.get('/get/brand/:brand_name', getProductByBrand)
 
 export default router;
