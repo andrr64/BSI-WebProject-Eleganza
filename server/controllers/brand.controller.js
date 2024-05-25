d1074a5f21ae617ab0c7a673840f8949191dafbb
andrr64 <derzandrr03@gmail.com>
2024-05-26 01:04:16 +0700

    penambahan fungsi updateBrand dan penyederhanaan kode

--- server/controllers/brand.controller.js
+++ server/controllers/brand.controller.js
@@ -1,5 +1,5 @@
 import { Brand } from "../models/brand.model.js";
-import { serverNotFound, serverOk } from "./response.controller.js";
+import { serverNotFound, serverOk, serverBadRequest, serverInternalError } from "./response.controller.js";
 import { MESSAGE, serverLog } from "./server.log.controller.js";
 import { serverProcess } from "./server.process.controller.js";
 
@@ -8,51 +8,52 @@ import { serverProcess } from "./server.process.controller.js";
  * @param {Object} req - Objek request dari client.
  * @param {Object} res - Objek response untuk dikirim kembali ke client.
  */
-export const createBrand = async(req, res) => {
+export const createBrand = async (req, res) => {
     await serverProcess(res, async () => {
-        serverLog(MESSAGE.STARTOF_REQUEST, 'new request: create brand');
-        const {title, icon, description} = req.body;
-        const newBrand = new Brand({
-            title: title,
-            description: description,
-            icon: icon
-        });
+        const { title, icon, description } = req.body;
+        const newBrand = new Brand({ title, icon, description });
         await newBrand.save();
-        serverLog(MESSAGE.OK);
-        serverOk(res, 'OK');
+        serverOk(res, 'Brand created successfully');
     }, 'create brand');
-}
+};
 
 /**
  * Mengambil semua brand dari database dan mengirimnya kembali ke client.
  * @param {Object} req - Objek request dari client.
  * @param {Object} res - Objek response untuk dikirim kembali ke client.
  */
-export const getBrands = async(req, res) => {
+export const getBrands = async (req, res) => {
     await serverProcess(res, async () => {
         const brands = await Brand.find({});
-        if (brands.length === 0){
-            serverNotFound(res);
-            return;
-        }
-        serverLog(MESSAGE.SENDING);
+        if (brands.length === 0) return serverNotFound(res, 'No brands found');
         serverOk(res, brands);
-        serverLog(MESSAGE.OK);
     }, 'get brands');
-}
+};
 
 /**
  * Mengambil brand berdasarkan ID dari database dan mengirimnya kembali ke client.
  * @param {Object} req - Objek request dari client.
  * @param {Object} res - Objek response untuk dikirim kembali ke client.
  */
-export const getBrandById = async(req, res) => {
+export const getBrandById = async (req, res) => {
     await serverProcess(res, async () => {
         const brand = await Brand.findById(req.params.id);
-        if (brand){
-            serverOk(res, brand);
-            return;
-        }
-        serverNotFound(res);
+        if (!brand) return serverNotFound(res, 'Brand not found');
+        serverOk(res, brand);
     }, 'get brand by id');
-}
+};
+
+/**
+ * Mengupdate brand yang ada di database berdasarkan ID.
+ * @param {Object} req - Objek request dari client.
+ * @param {Object} res - Objek response untuk dikirim kembali ke client.
+ */
+export const updateBrand = async (req, res) => {
+    await serverProcess(res, async () => {
+        const { title, icon, description } = req.body;
+        const updateData = { title, icon, description };
+        const updatedBrand = await Brand.findByIdAndUpdate(req.params.id, updateData, { new: true });
+        if (!updatedBrand) return serverNotFound(res, 'Brand not found');
+        serverOk(res, updatedBrand);
+    }, 'update brand');
+};
