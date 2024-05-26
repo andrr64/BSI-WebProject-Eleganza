300a7ae8bea618b61712fdc41743cdf445795347
andrr64 <derzandrr03@gmail.com>
2024-05-26 22:01:50 +0700

    Add new route

--- web/client/src/AppRoute.jsx
+++ web/client/src/AppRoute.jsx
@@ -7,5 +7,29 @@ export const ROUTE = {
         signin: '/user/signin',
         signup: '/user/signup',
         profile: '/user/profile'
+    },
+    collection_brand: '/collection/brand/:brand_name',
+    collection_gender: '/collection/gender/:gender',
+    collection_category: '/collection/category/:category',
+}
+
+export function goToCollection(collectionName){
+    return `/collection/${collectionName}`
+}
+
+export function goToBrandCollection(brandName){
+    return `/collection/brand/${brandName}`
+}
+
+export function goToAXCollection(a, X){
+    return `/collection/${a}/${X}`;
+}
+
+export function goToNXCollection(pathArr, x) {
+    let url = '/collection';
+    for (let i = 0; i < pathArr.length; i++) {
+        console.log(pathArr[i]);
+        url = `${url}/${pathArr[i]}`;
     }
+    return `${url}/${x}`;
 }
