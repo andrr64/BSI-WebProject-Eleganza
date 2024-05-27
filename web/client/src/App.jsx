de3f77d8b45f6854bbd5be20520fa54289199973
andrr64 <derzandrr03@gmail.com>
2024-05-27 20:56:19 +0700

    remove route

--- web/client/src/App.jsx
+++ web/client/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { SignInController } from "./pages/user/SignInController";
 import { SignUpController } from "./pages/user/SignUpController";
 import {ROUTE} from "./AppRoute";
@@ -8,21 +8,13 @@ import Woman from "./pages/Woman";
 import ProfileController from "./pages/user/ProfileController";
 import NotFound from "./pages/NotFound";
 import Experiment from "./pages/Experiment";
-import NavigationBar from "./components/navbar/NavigationBar";
-
-function NavigationBarControl() {
-  const location = useLocation();
-  if (
-    location.pathname === ROUTE.user.signin ||
-    location.pathname === ROUTE.user.signup
-  ) return null;
-  return <NavigationBar/>; 
-}
+import CollectionCategory from "./pages/collection/Category";
+import CollectionGender from "./pages/collection/Gender";
+import CollectionBrand from "./pages/collection/Brand";
 
 function App() {
   return (
     <BrowserRouter>
-      <NavigationBarControl/>
       <Routes>
         <Route path={ROUTE.homepage} element={<Homepage />} />
         <Route path={ROUTE.man} element={<Man />} />
@@ -30,7 +22,11 @@ function App() {
         <Route path={ROUTE.user.signin} element={<SignInController />} />
         <Route path={ROUTE.user.signup} element={<SignUpController />} />
         <Route path={ROUTE.user.profile} element={<ProfileController/>} />
+        <Route path={ROUTE.collection_brand} element={<CollectionBrand/>} />
+        <Route path={ROUTE.collection_category} element={<CollectionCategory/>} />
+        <Route path={ROUTE.collection_gender} element={<CollectionGender/>} />
         <Route path='*' element={<NotFound/>} /> {/* Rute untuk URL tidak valid*/}
+        <Route path="/notfound" element={<NotFound/>} />
         <Route path="/experiment" element={<Experiment/>} />
       </Routes>
     </BrowserRouter>
