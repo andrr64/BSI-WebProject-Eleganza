e5cbe75bcb3225b6672ac63202e76418e853fb98
andrr64 <derzandrr03@gmail.com>
2024-05-28 02:22:29 +0700

    update configuration

--- web/client/vite.config.js
+++ web/client/vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:{
+    proxy: {
       '/api': {
-        target: 'http://localhost:3000', // Atur target ke URL server backend Anda
-        secure: false
+        target: 'http://localhost:3000',
+        secure: false,
       },
-    }
+    },
   },
   plugins: [react()],
-})
+});
