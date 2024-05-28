853e0ff27ce89794ef663f147aebaebe6fbc5b51
andrr64 <derzandrr03@gmail.com>
2024-05-28 12:07:50 +0700

    Update UI

--- client/src/pages/ServerError.jsx
+++ client/src/pages/ServerError.jsx
@@ -1,8 +1,4 @@
-import { useNavigate } from "react-router-dom"
-import { ROUTE } from "../AppRoute";
-
 function ServerError() {
-  const navigate = useNavigate();
   return (
     <>
       <div className="w-screen h-screen flex justify-center items-center">
@@ -10,16 +6,12 @@ function ServerError() {
         </p>
         <p className="text-center">
           <span className="font-inter-light text-lg">
-            Your Connection OK. But...
+            You are trying to connect to the server. But...
           </span>
           <br />
           <span className="text-3xl">
             <b>500</b> <span className="font-inter-light">Internal Server Error</span>
           </span>
-          <br />
-          <button className="text-white bg-black py-2 px-5 mt-3 rounded-lg" onClick={() => navigate(ROUTE.homepage)}>
-            Refresh
-          </button>
         </p>
       </div>
     </>
