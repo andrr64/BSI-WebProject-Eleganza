f351b3801b747ed07e3bcf64af9834c9f1ca3ccd
andrr64 <derzandrr03@gmail.com>
2024-05-28 19:45:50 +0700

    add lottie

--- client/src/pages/RenderPage.jsx
+++ client/src/pages/RenderPage.jsx
@@ -1,11 +1,24 @@
 import Content from "./Content";
 import ServerError from "./ServerError";
+import Lottie from "react-lottie";
+import loadingAnimation from "../assets/lotties/loading";
 
 export  default  function Page(loading, server_status, render_function){
+  const lottie_loading_opt = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingAnimation,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  }
+
   if (loading && server_status){
     return (
       <div className="w-screen h-screen flex items-center justify-center">
-        x
+        <div className="w-28 md:w-52">
+          <Lottie options={lottie_loading_opt}/>
+        </div>
       </div>
     )
   } 
