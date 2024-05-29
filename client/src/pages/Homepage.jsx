020b901f3f1f4c7fdc11aaa5d361c29d14410b74
andrr64 <derzandrr03@gmail.com>
2024-05-30 03:30:52 +0700

    Update Homepage.jsx

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -4,12 +4,13 @@ import { useEffect, useState } from "react";
 import { serverApiJsonGet, isServerOnline } from "../api/API";
 import { goToNXCollection } from "../AppRoute";
 import { scrollToZero } from "../utility/ScrollToZero";
+import { delay } from "../utility/Delay";
 import Page from "./RenderPage";
 
 function Title ({judul}){
   return (
-    <div className="flex justify-center items-center py-5">
-      <h1 className="text-black uppercase text-lg md:text-2xl font-inter-light ">
+    <div className="flex justify-center items-center">
+      <h1 className="text-black uppercase text-lg md:text-xl font-inter-light">
       {judul}
       </h1>
     </div>
@@ -19,9 +20,11 @@ function Title ({judul}){
 function Section({id, title, layout, data}){
   return (
     <section id={id}>
-      <Title judul={title}/>
+      <div className="py-5">
+        <Title judul={title}/>
+      </div>
       <div className={`${layout}`}>
-      {
+        {
           data.map((item, index) => {
             return (
               <CollectionCard 
@@ -59,39 +62,36 @@ function Homepage() {
   const mainContent = () => {
     return (
       <>
-        <section id="content" className={`mx-10 my-20 $ ${loading? 'h-screen' : ''}`}>
-          <Section 
-            id={"merek-unggulan"} 
-            title={kontenMerekUnggulan.title} 
-            layout={`grid grid-cols-1 md:grid-cols-3 px-5 gap-2 w-full h-1/2 mb-5`}
-            data={kontenMerekUnggulan.data}
-          />
-          <Section 
-            id={"gender"} 
-            title={kontenGender.title} 
-            layout={`grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-1/2 mb-5`}
-            data={kontenGender.data}
-          />
-          <Section 
-            id={"kategori"} 
-            title={kontentKategori.title} 
-            layout={`grid justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2`}
-            data={kontentKategori.data}
-          />
+        <section id="content" className={`mx-2 my-20 md:mx-20 ${loading? 'h-screen' : ''}`}>
+            <Section 
+              id={"merek-unggulan"} 
+              title={kontenMerekUnggulan.title} 
+              layout={`grid grid-cols-2 md:grid-cols-3 gap-2 px-5 w-full h-1/2`}
+              data={kontenMerekUnggulan.data}
+            />
+            <Section 
+              id={"gender"} 
+              title={kontenGender.title} 
+              layout={`grid grid-cols-1 md:grid-cols-2 gap-2 w-full h-1/2`}
+              data={kontenGender.data}
+            />
+            <Section 
+              id={"kategori"} 
+              title={kontentKategori.title} 
+              layout={`grid justify-center items-center grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-2`}
+              data={kontentKategori.data}
+            />
         </section>
       </>
     ); 
   }
 
   useEffect(() => {
-    const delay = (ms) => {
-      return new Promise(resolve => setTimeout(resolve, ms));
-    }
-
     const getContents = async () => {
       setLoading(true);
-      setServerStatus(await isServerOnline());
       try {
+        if (!(await isServerOnline()))
+          return setServerStatus(false);
         const merek_unggulan = await (await serverApiJsonGet('/web/homepage/merek-unggulan')).json();
         const kategori = await (await serverApiJsonGet('/web/homepage/kategori')).json();
         const gender = await (await serverApiJsonGet('/web/homepage/gender')).json();
@@ -100,8 +100,7 @@ function Homepage() {
         setKontenGender(gender.data);
         await delay(500);
       } catch (error) {
-        console.log(error);
-        setServerStatus(false);
+        console.log(error.message);
       } finally {
         setLoading(false);
       }
@@ -110,7 +109,7 @@ function Homepage() {
     getContents();
   }, []);
 
-  return Page(loading, serverStatus, mainContent);
+  return Page(loading, serverStatus, mainContent, true, true);
 }
 
 export default Homepage;
