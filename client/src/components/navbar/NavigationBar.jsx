8659381725235d25d114cbcd18f25dc0115a1e75
andrr64 <derzandrr03@gmail.com>
2024-05-29 15:14:17 +0700

    new logic: can change mode

--- client/src/components/navbar/NavigationBar.jsx
+++ client/src/components/navbar/NavigationBar.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import ProfileMenu from './ProfileMenu';
 import SearchBar from "./SearchBar"
 import MenuBar from "./MenuBar";
@@ -5,8 +6,20 @@ import { Link } from "react-router-dom"
 import { ROUTE } from "../../AppRoute";
 
 import './NavigationBar.css'
-
-const NavigationBar = () => {
+const TypeWhite = () => {
+  return (
+    <div className="grid grid-cols-3 items-center justify-between p-5">
+    <div className="flex gap-5">
+      <Link>Fashion Accessories</Link>
+      <Link>Fragrance & Beauty</Link>
+    </div>
+    <div className="text-center">
+      <span className="font-laBelle text-4xl">Eleganza</span>
+    </div>
+  </div>
+  )
+}
+const NavigationBar = ({type='default'}) => {
   // const createNavBarItem = (title, path) => {
   //   return {
   //     title: title,
@@ -19,7 +32,9 @@ const NavigationBar = () => {
   //   createNavBarItem('Wanita', ROUTE.woman),
   //   createNavBarItem('Anak-Anak', ROUTE.children),
   // ]
-
+  if (type === 'white'){
+    return  <TypeWhite/>
+  }
   return (
     <nav className="bg-black text-white py-5 px-8 md:px-10 lg:px-14 flex items-center navigationBar">
       <div className="flex items-center flex-grow">  {/* Flex-grow for spacing */}
