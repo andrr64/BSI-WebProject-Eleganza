c8c5e77115ba4e076c349ed88dd2e0b243d623ee
andrr64 <derzandrr03@gmail.com>
2024-05-29 15:14:28 +0700

    update logic

--- client/src/pages/RenderPage.jsx
+++ client/src/pages/RenderPage.jsx
@@ -3,7 +3,7 @@ import ServerError from "./ServerError";
 import Lottie from "react-lottie";
 import loadingAnimation from "../assets/lotties/loading";
 
-export  default  function Page(loading, server_status, render_function){
+const _loadingAnimation = () => {
   const lottie_loading_opt = {
     loop: true,
     autoplay: true,
@@ -12,19 +12,20 @@ export  default  function Page(loading, server_status, render_function){
       preserveAspectRatio: "xMidYMid slice"
     }
   }
-
-  if (loading && server_status){
-    return (
-      <div className="w-screen h-screen flex items-center justify-center">
-        <div className="w-28 md:w-52">
-          <Lottie options={lottie_loading_opt}/>
-        </div>
+  return (
+    <div className="w-screen mt-5 h-screen flex items-center justify-center">
+      <div className="w-28 md:w-52">
+        <Lottie options={lottie_loading_opt}/>
       </div>
-    )
-  } 
+    </div>
+  )
+}
 
-  else if (!server_status && !loading){
+export  default function Page(loading, server_status, render_function, nav, footer){
+  if (loading && server_status){
+    return _loadingAnimation();
+  } else if (!server_status){
     return <ServerError/>;  
   }
-  return <Content main={render_function()}/>;
+  return <Content nav={nav} footer={footer} main={render_function()}/>;
 }
