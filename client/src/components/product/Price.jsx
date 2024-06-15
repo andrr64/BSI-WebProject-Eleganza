e9869f1a7261af0025659a4b18274950855b253e
andreas64 <derzandrr03@gmail.com>
2024-06-16 00:20:26 +0700

    ubah ke bahasa indonesia

--- client/src/components/product/Price.jsx
+++ client/src/components/product/Price.jsx
@@ -3,9 +3,9 @@ import { formatRupiah } from '../../utility/Format'
 
 export default function ProductPrice({product}) {
   return (
-    <div className="flex items-baseline space-x-2 my-4">
+    <div className="flex space-x-2 my-4 items-center">
         <p className="text-3xl font-semibold text-black">{formatRupiah(product.price)}</p>
-        <span className="px-2 py-1 text-sm rounded-full bg-green-100 text-green-800">In stock</span>
+        <span className="px-2 py-1 text-sm rounded-full bg-green-100 text-green-800">Stok Tersedia</span>
     </div>
   )
 }
