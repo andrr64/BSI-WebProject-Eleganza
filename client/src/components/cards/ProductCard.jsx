6f2aba1b6ce761fb8e6be47b570b9cd0fc76ab9e
andreas64 <derzandrr03@gmail.com>
2024-06-17 00:39:52 +0700

    update desain UI

--- client/src/components/cards/ProductCard.jsx
+++ client/src/components/cards/ProductCard.jsx
@@ -1,32 +1,63 @@
 import { Link } from "react-router-dom"
 import { goToProductDetail } from "../../AppRoute"
 import { formatRupiah } from "../../utility/Format"
+import { IconCartPlus } from "../Icons";
 
 /* eslint-disable react/prop-types */
-function ProductCard({data}) {
+
+// Fungsi untuk menampilkan gambar produk
+function ProductImage({ product, src, alt }) {
     return (
-        <div className="grid-cols cols-1 border w-full rounded-lg overflow-hidden">
-            <Link to={goToProductDetail(data._id)}>
-                <img className="h-10/12 w-full object-cover" src={data.list_picture[0]} alt={`Picture of ${data.name}`} />
+        <Link to={goToProductDetail(product._id)}>
+            <img className="h-10/12 w-full object-cover" src={src} alt={alt} />
+        </Link>
+    );
+}
+
+// Fungsi untuk menampilkan informasi produk
+function ProductInfo({ category, name, price, id }) {
+    return (
+        <div className="text-center py-5">
+            <Link className="underline">
+                <p className="text-center text-sm font-inter-light mb-2 uppercase">{category}</p>  
             </Link>
-            <div className="text-center py-5">
-                <Link to={'/'}>
-                    <p className="text-center font-inter-light mb-2 uppercase">{data.category}</p>  
-                </Link>
-                <Link to={goToProductDetail(data._id)}>
-                    <p className="font-inter font-bold">{data.name}</p>
-                </Link>
-                <p className="font-inter uppercase text-lg">{formatRupiah(data.price)}</p>
-            </div>
-            <Link>
-                <div className="text-center mb-5">
-                    <div className="inline bg-black py-2 px-5 rounded-sm text-white">
-                        Add to Cart
-                    </div>
-                </div>
+            <Link to={goToProductDetail(id)}>
+                <p className="font-inter font-bold text-sm">{name}</p>
             </Link>
+            <p className="font-inter uppercase text-base">{formatRupiah(price)}</p>
         </div>
+    );
+}
+
+const renderButton = (text, icon, callback) => {
+    return (
+        <button onClick={() => callback()} className="hover:bg-black bg-gray-50 hover:text-white transition ease-in-out duration-300 border text-slate-700 flex rounded-lg items-center justify-center px-5 py-2 gap-2">
+            {icon}
+            {text}
+        </button>
     )
 }
 
-export default ProductCard
+// Fungsi untuk tombol "Add to Cart"
+function Actions() {
+    return (
+        <div className="w-full gap-2 flex justify-center ">
+            {renderButton('Tambah', <IconCartPlus size="18"/>, ()=>{})}
+        </div>
+    );
+}
+
+// Fungsi utama untuk kartu produk
+function ProductCard({ data }) {
+    return (
+        <div className="hover:bg-zinc-100 grid-cols cols-1 hover:scale-105 transition duration-300 ease-in-out border border-slate-200 w-full rounded-lg overflow-hidden">
+            <ProductImage product={data} src={data.list_picture[0]} alt={`Picture of ${data.name}`} />
+            <div className="mb-5">
+                <ProductInfo category={data.category} name={data.name} price={data.price} id={data._id} />
+                <Actions />
+            </div>
+        </div>
+    );
+}
+
+export default ProductCard;
--- client/src/components/product/Actions.jsx
+++ client/src/components/product/Actions.jsx
@@ -1,10 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IconCartPlus, IconCatatan } from '../Icons'
 
 export default function ProductActions() {
+
+  const [stok, setStok] = useState(1);
+  const [note, setNote] = useState('')
+
+  const handleStokChange = (event, availableStock) => {
+    const value = event.target.value;
+    if (!isNaN(value) && value <= availableStock) {
+      setStok(Number(value));
+    }
+  };
+
+  const handleNoteChange = (e) => {
+    setNote(e.target.value);
+  }
+
+  const renderFieldStok = (stokTersedia) => {
+    return (
+      <>
+        <div className='grid grid-cols-2 divide-x'>
+          <div className='flex items-center gap-4'>
+            <input type="text"  value={stok} placeholder="Stok" className={`font-bold input input-bordered input-md w-16 ${stok <= 0 && 'input-error'}`} onChange={(e) => handleStokChange(e, stokTersedia)} />
+            <p>Stok Tersedia: {stokTersedia}</p>
+          </div>
+          <div className='px-5 space-y-2'>
+              <label htmlFor="" className='flex items-center gap-2'>
+                <IconCatatan/>
+                Catatan
+              </label>
+              <textarea onChange={handleNoteChange} value={note} className="font-bold textarea h-24 textarea-bordered w-full" placeholder="Ketik disini"></textarea>
+          </div>
+        </div>
+        <p className='h-4 text-red text-sm text-red-400'>
+          {stok <= 0 && 'Min. pembelian produk adalah 1 barang'}
+        </p>  
+      </>
+    )
+  }
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-3 mt-8">
-        <button className="w-full p-3 border border-gray-200 bg-gray-50 rounded-md text-gray-800">Add to cart</button>
-        <button className="w-full p-3 bg-blue-600 border border-blue-600 rounded-md text-white">Buy now</button>
+    <div className="p-5 grid grid-cols-1 border gap-3 mt-8">
+      <h2 className='font-bold'>Atur jumlah dan catatan</h2>
+      {renderFieldStok(20)}
+      <div className='grid grid-cols-2 gap-5'>
+        <button className="w-full p-3 border border-gray-200 bg-gray-50 rounded-md text-gray-800 flex items-center justify-center gap-4 transition ease-in-out duration-300  hover:bg-gray-200">
+          <IconCartPlus />
+          Masukkan Keranjang
+        </button>
+        <button className="w-full p-3 bg-blue-600 border border-blue-600 rounded-md text-white">Beli Sekarang</button>
+      </div>
     </div>
   )
 }
