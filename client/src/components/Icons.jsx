22b796fd36da9eb2fadda81bda8f9efd016a9335
andreas64 <derzandrr03@gmail.com>
2024-06-17 00:39:39 +0700

    add new icons: note, chat

--- client/src/components/Icons.jsx
+++ client/src/components/Icons.jsx
@@ -3,9 +3,11 @@ import { IoNavigateOutline } from "react-icons/io5";
 import { IoChatbox } from "react-icons/io5";
 import { FaShoppingCart } from "react-icons/fa";
 import { IoIosChatbubbles } from "react-icons/io";
+import { SlNote } from "react-icons/sl";
 
 export const IconCartPlus = ({size = ''}) => (<FaCartPlus size={size}/>)
 export const IconDetail = ({size = ''}) => (< IoNavigateOutline size={size}/>)
 export const IconCart = ({size = ''}) => (<FaShoppingCart   size={size} />)
 export const IconChat = ({size = ''}) => (<IoChatbox size={size} />)
 export const IconChatBubble = ({size = ''}) => (<IoIosChatbubbles size={size} />)
+export const IconCatatan = ({size = ''}) => (<SlNote size={size} />)
