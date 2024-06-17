8728f477c1acbabed79d9667db23f0bf793a605a
andreas64 <derzandrr03@gmail.com>
2024-06-18 00:46:50 +0700

    add shcdnui + comp: Toast

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from "path"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
@@ -12,4 +12,9 @@ export default defineConfig({
     },
   },
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
