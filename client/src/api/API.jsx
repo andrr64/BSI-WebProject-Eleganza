019099bccd16c2178640b872c0194d31063129cb
andreas64 <derzandrr03@gmail.com>
2024-06-18 15:01:38 +0700

    add serverGetItemLength

--- client/src/api/API.jsx
+++ client/src/api/API.jsx
@@ -60,3 +60,9 @@ export async function addItemToCart(user_id, item) {
     const response = await serverApiJsonPost(apiEndpoint, item);
     return response.json();
 }
+
+export async function serverGetItemsLength(user_id) {
+    const apiEndpoint = `/user/cart/length/${user_id}`;
+    const response = await serverApiJsonGet(apiEndpoint, {});
+    return response.json();
+}
