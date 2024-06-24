72888ea9c2c734a18dee8b57828989a05f896c2b
andreas64 <derzandrr03@gmail.com>
2024-06-24 21:38:34 +0700

    add searchbar

--- client/src/components/navbar/SearchBar.jsx
+++ client/src/components/navbar/SearchBar.jsx
@@ -1,12 +1,39 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { faClose, faSearch } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
+
+
+const _STYLE_container = `
+bg-gray-600 hover:bg-gray-700 cursor-pointer 
+rounded-full px-4 py-3 flex items-center
+`
+
+const _STYLE_inputText = `
+  bg-transparent 
+  mx-3 w-full border:none outline-none
+  font-inter text-sm 
+`
 
 const SearchBar = () => {
+  const [text, setText] = useState('');
+
+  const handleSubmit = () => {
+    console.log(text);
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleSubmit();
+    }
+  };
+
   return (
-    <div>
-      <button>
-        <FontAwesomeIcon icon={faSearch} color="black" size="lg"/>
-      </button>
+    <div className={_STYLE_container}>
+      <FontAwesomeIcon icon={faSearch} color="gray-500" size="lg" />
+      <input onKeyDown={handleKeyDown} value={text} onChange={(e) => setText(e.target.value)} placeholder="Cari disini..." type="text" className={_STYLE_inputText} />
+      {text.length !== 0 && (
+        <FontAwesomeIcon icon={faClose} color="gray-500" size="lg" onClick={(e) => setText('')} />
+      )}
     </div>
   );
 };
